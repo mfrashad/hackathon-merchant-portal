23b6661fb129d8b65357c872023838064b1678a5
mfrashad <m.fathyrashad@gmail.com>
2018-10-07 07:16:31 +0800

    Rename profits to sales

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,11 +2,11 @@ class Company < ApplicationRecord
   has_many :users
   has_many :payments
 
-  def total_profits
+  def total_sales
     self.payments.reduce(0) { |sum, payment| sum+payment.amount }
   end
 
   def total_payable_amount
-    total_profits * COMMISSION
+    total_sales * COMMISSION
   end
 end
